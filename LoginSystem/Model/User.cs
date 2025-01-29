71c382970d842985b4565bf984536f73445644af
Eduardo <eduardosume082@gmail.com>
2025-01-29 17:11:03 -0300

    add constructor user class

--- LoginSystem/Model/User.cs
+++ LoginSystem/Model/User.cs
@@ -8,13 +8,23 @@ namespace LoginSystem.Model
 {
     public class User
     {
+
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string role { get; set; } = "user";
+        public string Role { get; set; } = "user";
         public DateTime CreatedAt { get; set; }
 
+        public User(int id, string username, string email, string password, string role, DateTime createdAt)
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            Password = password;
+            this.Role = role;
+            CreatedAt = createdAt;
+        }
 
     }
 }
